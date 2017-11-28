b184506da48bebdbf92ff1d44ced193565929d9c
ElNino Wang <elninowang@gmail.com>
2017-11-28 22:50:09 +0800

    再次调整 js代码，让最清楚

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,16 @@
 
-/** Silicon Valley */
+/** 硅谷中心坐标，也是这个项目的起始坐标 */
 const start_point = {lat: 37.387474, lng: -122.057543};
 
 //const wikipedia_api_url = "http://zh.wikipedia.org/w/api.php?&action=query&titles=%E7%A1%85%E8%B0%B7&format=json&prop=revisions&rvprop=content";
+
 const wikipedia_api_url = "https://free-api.heweather.com/s6/weather/forecast?location=%E5%9C%A3%E4%BD%95%E5%A1%9E&key=7959b2d195af4b38a2afe26eccd7493f&lang=cn&unit=m";
 
+let map;
+
+/**
+ * 初始话地图，是由google map API在加载完成后直接调用
+ */
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map($('#map').get(0), {
@@ -14,19 +20,24 @@ function initMap() {
         mapTypeControl: false,
     });
 
+    /** 启动 knockoutjs 的 MVVM 模式*/
     ko.applyBindings(new ViewModel());
 }
 
-// ViewModel
 let ViewModel = function () {
     let self = this;
 
+    /** 绑定输入框 */
     this.current = ko.observable();
+    /** 绑定是否显示poi图标复选框 */
     this.use_poi_icon = ko.observable();
+    /** 绑定搜索结果列表 */
     this.markers = ko.observableArray([]);
 
+    /** 地图中的弹出框 */
     this.infoWindow = new google.maps.InfoWindow();
 
+    /** 中心点标记 */
     this.centerMarker = new google.maps.Marker({
         position: start_point,
         title: "硅谷",
@@ -34,37 +45,41 @@ let ViewModel = function () {
         id: 1,
     });
 
-    (function() {
-        map.addListener('bounds_changed', function(){
+    /** ViewModel类的启动函数 */
+    (() => {
+        map.addListener('bounds_changed', () => {
             let autocomplete = new google.maps.places.Autocomplete(
                 $('#search-place').get(0),
                 {bounds: map.getBounds()}
             );
-            autocomplete.addListener('place_changed', function(){
+            autocomplete.addListener('place_changed', () => {
                 let place = autocomplete.getPlace();
                 self.searchPlaces(place.formatted_address);
             });
         });
 
-        self.centerMarker.addListener('click', function () {
-            self.showCenterInfoWindow(this, self.infoWindow);
+        self.centerMarker.addListener('click', () => {
+            self.showCenterInfoWindow(self.centerMarker, self.infoWindow);
         });
 
         self.centerMarker.setMap(map);
     })();
-    
+
+    /**
+     * 显示 中心点标记 的弹出框，调用第三方API
+     */
     this.showCenterInfoWindow = function(marker, infoWindow) {
         if (infoWindow.marker != marker) {
             infoWindow.marker = marker;
-            fetch("http://api.map.baidu.com/telematics/v3/weather?location=%E5%8C%97%E4%BA%AC&output=json&ak=BxHHQAtW62yaGYTQnDjhlethS8brEAzf", {mode: "no-cors"})
-            fetch("https://api.foursquare.com/v2/venues/4d9a662a674ca14376eaba43?client_id=NONGGLXBKX5VFFIKKEK1HXQPFAFVMEBTRXBWJUPEN4K14JUE&client_secret=ZZDD1SLJ4PA2X4AJ4V23OOZ53UM4SFZX0KORGWP5TZDK4YYJ&v=20130815")
+
+            let html = `<div><h1>硅谷-天气</h1><br><p>正在获取数据 </p></div>`;
+            infoWindow.setContent(html);
+            infoWindow.open(map, marker);
+
             fetch(wikipedia_api_url)
-            .then(response => response.json()) 
+            .then(response => response.json())  
             .then(obj => {
                 forecast = obj.HeWeather6[0].daily_forecast;
-                console.log(forecast[0]);
-                console.log(forecast[1]);
-                console.log(forecast[2]);
 
                 html = `<div><h1>硅谷-天气</h1><ul>`;
                 forecast.forEach(f => {
@@ -74,19 +89,25 @@ let ViewModel = function () {
                 console.log(html);
 
                 infoWindow.setContent(html);
-
-                infoWindow.open(map, marker);
-                infoWindow.addListener('closeclick', function () {
-                    infoWindow.marker = null;
-                });
             })
             .catch(error => {
-                console.log(error);
+                // 获取错误
+                console.error(error);
+                html = `<div>`
+                    + `<h1>硅谷-天气</h1><br><p>获取错误: </p>`
+                    + `<span>${error}</span>`
+                    + `</div>`
+                infoWindow.setContent(html);
             });
-            
+            infoWindow.addListener('closeclick', () => {
+                infoWindow.marker = null;
+            });
         }
     }
 
+    /**
+     * 搜索地址
+     */
     this.search = function () {
         let address = this.current().trim();
         if (address) {
@@ -108,12 +129,17 @@ let ViewModel = function () {
         placesService.textSearch({
             query: address,
             bounds: bounds
-        }, function (results, status) {
+        }, (results, status) => {
             if (status === google.maps.places.PlacesServiceStatus.OK) {
                 self.createMarkersForPlaces(results);
             }
         });
     }
+
+    /**
+     * 给搜索出来的地址，在地图中添加标记
+     * @param {*} places 
+     */
 
     this.createMarkersForPlaces = function(places) {
         let bounds = new google.maps.LatLngBounds();
@@ -121,7 +147,7 @@ let ViewModel = function () {
             let place = places[i];
             let marker;
             if (self.use_poi_icon()) {
-                // Create a marker for each place.
+                // 地图标记的时候，显示POI图标
                 marker = new google.maps.Marker({
                     map: map,
                     icon: {
@@ -136,6 +162,7 @@ let ViewModel = function () {
                     id: place.place_id,
                 });
             } else {
+                // 地图标记的时候，显示Google地图默认图标
                 marker = new google.maps.Marker({
                     map: map,
                     title: place.name,
@@ -144,12 +171,12 @@ let ViewModel = function () {
                 });
             }
     
-            marker.addListener('click', function () {
-                self.populateInfoWindow(this, self.infoWindow);
+            // 设置每个点点击弹出详细内容
+            marker.addListener('click', () => {
+                self.populateInfoWindow(marker, self.infoWindow);
             });
             self.markers.push(marker);
             if (place.geometry.viewport) {
-                // Only geocodes have viewport.
                 bounds.union(place.geometry.viewport);
             } else {
                 bounds.extend(place.geometry.location);
@@ -158,57 +185,68 @@ let ViewModel = function () {
         map.fitBounds(bounds);
     }
 
+    /**
+     * 清楚所有标记和弹出框
+     */
     this.clearMarkers = function() {
         console.log('clearMarkers');
-        self.markers().forEach( function(marker) {
-            marker.setMap(null)
-        });
+        self.markers().forEach(marker => marker.setMap(null));
         self.markers.removeAll();
-        self.infoWindow.marker = null;
+
+        self.centerMarker.setMap(null);
     }
 
-    this.showPlaceInfo = function (marker) {
+    /**
+     * 绑定knockoutjs里面 当用户点击地址列表中的一项时
+     *  @param {google.maps.Marker} marker 用户点击的项对应地图中的哪一个标记
+     */
+    this.showPlaceInfo = function(marker) {
         google.maps.event.trigger(marker, 'click');
         $('#nav').removeClass('open');  
     }.bind(this);
 
+    /**
+     * 
+     * @param {google.maps.Marker} marker  需要弹出详细内容的标记
+     * @param {google.maps.InfoWindow} infoWindow  弹出框对象
+     */
+
     this.populateInfoWindow = function(marker, infoWindow) {
         if (infoWindow.marker != marker) {
-            //infoWindow.marker = null;
             infoWindow.marker = marker;
 
             var service = new google.maps.places.PlacesService(map);
             service.getDetails({
                 placeId: marker.id
-            }, function (place, status) {
+            }, (place, status) => {
                 if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    // Set the marker property on this infoWindow so it isn't created again.
-                    var innerHTML = '<div>';
+                    // 设置弹出框的内容
+                    var html = '<div>';
                     if (place.name) {
-                        innerHTML += '<strong>' + place.name + '</strong>';
+                        html += `<strong>${place.name}</strong>`;
                     }
                     if (place.formatted_address) {
-                        innerHTML += '<br>' + place.adr_address;
+                        html += `<br>${place.adr_address}`;
                     }
                     if (place.formatted_phone_number) {
-                        innerHTML += '<br>' + place.formatted_phone_number;
+                        html += `<br>${place.formatted_phone_number}`;
                     }
                     if (place.photos) {
-                        innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
-                            { maxHeight: 100, maxWidth: 200 }) + '">';
+                        html += `<br><br><img src="${place.photos[0].getUrl(
+                            { maxHeight: 100, maxWidth: 200 })}">`;
                     }
-                    innerHTML += '</div>';
-                    infoWindow.setContent(innerHTML);
+                    html += '</div>';
+                    infoWindow.setContent(html);
                     infoWindow.open(map, marker);
-                    // Make sure the marker property is cleared if the infoWindow is closed.
                 }
             });
-            infoWindow.addListener('closeclick', function () {
-                infoWindow.marker = null;
-            });
+            infoWindow.addListener('closeclick', () => infoWindow.marker = null);
         }
     };
 
+    /**
+     * 用户在移动模式下点击了 是否开启导航栏
+     */
     this.toggleNav = function() {
         $('#nav').toggleClass('open');  
         event.stopPropagation();
--- tmp/js/app.js
+++ tmp/js/app.js
@@ -1,38 +1,107 @@
-'use strict';
+"use strict";
 
-/** Phoenix */
-var start_point = { lat: 33.448377, lng: -112.074037 };
+/** 硅谷中心坐标，也是这个项目的起始坐标 */
+var start_point = { lat: 37.387474, lng: -122.057543 };
+
+//const wikipedia_api_url = "http://zh.wikipedia.org/w/api.php?&action=query&titles=%E7%A1%85%E8%B0%B7&format=json&prop=revisions&rvprop=content";
+
+var wikipedia_api_url = "https://free-api.heweather.com/s6/weather/forecast?location=%E5%9C%A3%E4%BD%95%E5%A1%9E&key=7959b2d195af4b38a2afe26eccd7493f&lang=cn&unit=m";
 
 var map = void 0;
 
+/**
+ * 初始话地图，是由google map API在加载完成后直接调用
+ */
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map($('#map').get(0), {
         center: start_point,
         styles: mapstyles,
-        zoom: 13,
+        zoom: 9,
         mapTypeControl: false
     });
 
+    /** 启动 knockoutjs 的 MVVM 模式*/
     ko.applyBindings(new ViewModel());
 }
 
-// ViewModel
 var ViewModel = function ViewModel() {
     var self = this;
 
+    /** 绑定输入框 */
     this.current = ko.observable();
+    /** 绑定是否显示poi图标复选框 */
     this.use_poi_icon = ko.observable();
+    /** 绑定搜索结果列表 */
     this.markers = ko.observableArray([]);
 
-    map.addListener('bounds_changed', function () {
-        var autocomplete = new google.maps.places.Autocomplete($('#search-place').get(0), { bounds: map.getBounds() });
-        autocomplete.addListener('place_changed', function () {
-            var place = autocomplete.getPlace();
-            self.searchPlaces(place.formatted_address);
-        });
+    /** 地图中的弹出框 */
+    this.infoWindow = new google.maps.InfoWindow();
+
+    /** 中心点标记 */
+    this.centerMarker = new google.maps.Marker({
+        position: start_point,
+        title: "硅谷",
+        animation: google.maps.Animation.DROP,
+        id: 1
     });
 
+    /** ViewModel类的启动函数 */
+    (function () {
+        map.addListener('bounds_changed', function () {
+            var autocomplete = new google.maps.places.Autocomplete($('#search-place').get(0), { bounds: map.getBounds() });
+            autocomplete.addListener('place_changed', function () {
+                var place = autocomplete.getPlace();
+                self.searchPlaces(place.formatted_address);
+            });
+        });
+
+        self.centerMarker.addListener('click', function () {
+            self.showCenterInfoWindow(self.centerMarker, self.infoWindow);
+        });
+
+        self.centerMarker.setMap(map);
+    })();
+
+    /**
+     * 显示 中心点标记 的弹出框，调用第三方API
+     */
+    this.showCenterInfoWindow = function (marker, infoWindow) {
+        if (infoWindow.marker != marker) {
+            infoWindow.marker = marker;
+
+            var html = "<div><h1>\u7845\u8C37-\u5929\u6C14</h1><br><p>\u6B63\u5728\u83B7\u53D6\u6570\u636E </p></div>";
+            infoWindow.setContent(html);
+            infoWindow.open(map, marker);
+
+            fetch(wikipedia_api_url).then(function (response) {
+                return response.json();
+            }).then(function (obj) {
+                forecast = obj.HeWeather6[0].daily_forecast;
+
+                html = "<div><h1>\u7845\u8C37-\u5929\u6C14</h1><ul>";
+                forecast.forEach(function (f) {
+                    html += "<li>" + f.date + ": \u767D\u5929" + f.cond_txt_d + "\uFF0C\u665A\u4E0A" + f.cond_txt_n + " \u6700\u4F4E" + f.tmp_min + "\u6444\u6C0F\u5EA6 \u6700\u9AD8" + f.tmp_max + "\u6444\u6C0F\u5EA6 " + f.wind_dir + " " + f.wind_sc + "</li>";
+                });
+                html += "</ul></div>";
+                console.log(html);
+
+                infoWindow.setContent(html);
+            }).catch(function (error) {
+                // 获取错误
+                console.error(error);
+                html = "<div>" + "<h1>\u7845\u8C37-\u5929\u6C14</h1><br><p>\u83B7\u53D6\u9519\u8BEF: </p>" + ("<span>" + error + "</span>") + "</div>";
+                infoWindow.setContent(html);
+            });
+            infoWindow.addListener('closeclick', function () {
+                infoWindow.marker = null;
+            });
+        }
+    };
+
+    /**
+     * 搜索地址
+     */
     this.search = function () {
         var address = this.current().trim();
         if (address) {
@@ -61,6 +130,11 @@ var ViewModel = function ViewModel() {
         });
     };
 
+    /**
+     * 给搜索出来的地址，在地图中添加标记
+     * @param {*} places 
+     */
+
     this.createMarkersForPlaces = function (places) {
         var bounds = new google.maps.LatLngBounds();
 
@@ -68,7 +142,7 @@ var ViewModel = function ViewModel() {
             var place = places[i];
             var marker = void 0;
             if (self.use_poi_icon()) {
-                // Create a marker for each place.
+                // 地图标记的时候，显示POI图标
                 marker = new google.maps.Marker({
                     map: map,
                     icon: {
@@ -83,6 +157,7 @@ var ViewModel = function ViewModel() {
                     id: place.place_id
                 });
             } else {
+                // 地图标记的时候，显示Google地图默认图标
                 marker = new google.maps.Marker({
                     map: map,
                     title: place.name,
@@ -91,13 +166,12 @@ var ViewModel = function ViewModel() {
                 });
             }
 
-            var placeInfoWindow = new google.maps.InfoWindow();
+            // 设置每个点点击弹出详细内容
             marker.addListener('click', function () {
-                self.populateInfoWindow(this, placeInfoWindow);
+                self.populateInfoWindow(marker, self.infoWindow);
             });
             self.markers.push(marker);
             if (place.geometry.viewport) {
-                // Only geocodes have viewport.
                 bounds.union(place.geometry.viewport);
             } else {
                 bounds.extend(place.geometry.location);
@@ -110,60 +184,71 @@ var ViewModel = function ViewModel() {
         map.fitBounds(bounds);
     };
 
+    /**
+     * 清楚所有标记和弹出框
+     */
     this.clearMarkers = function () {
-        //map.clearOverlays();
+        console.log('clearMarkers');
         self.markers().forEach(function (marker) {
-            marker.setMap(null);
+            return marker.setMap(null);
         });
         self.markers.removeAll();
+
+        self.centerMarker.setMap(null);
     };
 
-    self.showPlaceInfo = function (marker) {
+    /**
+     * 绑定knockoutjs里面 当用户点击地址列表中的一项时
+     *  @param {google.maps.Marker} marker 用户点击的项对应地图中的哪一个标记
+     */
+    this.showPlaceInfo = function (marker) {
         google.maps.event.trigger(marker, 'click');
         $('#nav').removeClass('open');
     }.bind(this);
 
-    this.populateInfoWindow = function (marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-            // In case the status is OK, which means the pano was found, compute the
-            // position of the streetview image, then calculate the heading, then get a
-            // panorama from that and set the options
-            var getStreetView = function getStreetView(data, status) {
-                if (status == google.maps.StreetViewStatus.OK) {
-                    var nearStreetViewLocation = data.location.latLng;
-                    var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
-                    infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-                    var panoramaOptions = {
-                        position: nearStreetViewLocation,
-                        pov: {
-                            heading: heading,
-                            pitch: 30
-                        }
-                    };
-                    var panorama = new google.maps.StreetViewPanorama($('#pano').get(0), panoramaOptions);
-                } else {
-                    infowindow.setContent('<div>' + marker.title + '</div>' + '<div>No Street View Found</div>');
+    /**
+     * 
+     * @param {google.maps.Marker} marker  需要弹出详细内容的标记
+     * @param {google.maps.InfoWindow} infoWindow  弹出框对象
+     */
+
+    this.populateInfoWindow = function (marker, infoWindow) {
+        if (infoWindow.marker != marker) {
+            infoWindow.marker = marker;
+
+            var service = new google.maps.places.PlacesService(map);
+            service.getDetails({
+                placeId: marker.id
+            }, function (place, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    // 设置弹出框的内容
+                    var html = '<div>';
+                    if (place.name) {
+                        html += "<strong>" + place.name + "</strong>";
+                    }
+                    if (place.formatted_address) {
+                        html += "<br>" + place.adr_address;
+                    }
+                    if (place.formatted_phone_number) {
+                        html += "<br>" + place.formatted_phone_number;
+                    }
+                    if (place.photos) {
+                        html += "<br><br><img src=\"" + place.photos[0].getUrl({ maxHeight: 100, maxWidth: 200 }) + "\">";
+                    }
+                    html += '</div>';
+                    infoWindow.setContent(html);
+                    infoWindow.open(map, marker);
                 }
-            };
-            // Use streetview service to get the closest streetview image within
-            // 50 meters of the markers position
-
-
-            // Clear the infowindow content to give the streetview time to load.
-            infowindow.setContent('');
-            infowindow.marker = marker;
-            // Make sure the marker property is cleared if the infowindow is closed.
-            infowindow.addListener('closeclick', function () {
-                infowindow.marker = null;
             });
-            var streetViewService = new google.maps.StreetViewService();
-            var radius = 50;streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-            // Open the infowindow on the correct marker.
-            infowindow.open(map, marker);
+            infoWindow.addListener('closeclick', function () {
+                return infoWindow.marker = null;
+            });
         }
     };
 
+    /**
+     * 用户在移动模式下点击了 是否开启导航栏
+     */
     this.toggleNav = function () {
         $('#nav').toggleClass('open');
         event.stopPropagation();
