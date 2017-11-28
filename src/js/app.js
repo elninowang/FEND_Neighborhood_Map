415a245315c3407894e04f1057009499b2be74e7
ElNino Wang <elninowang@gmail.com>
2017-11-28 11:56:11 +0800

    nav层的复现也绑定上去了

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 
-
 /** 美国 凤凰城 经纬度 Phoenix */
 const start_point = {lat: 33.448377, lng: -112.074037};
 
@@ -19,10 +18,11 @@ function initMap() {
 var ViewModel = function () {
     let self = this;
 
-    this.markers = [];
+    this.current = ko.observable();
+    this.use_poi_icon = ko.observable();
+    this.markers = ko.observableArray([]);
     
     map.addListener('bounds_changed', () => {
-        console.log("bounds = " + map.getBounds());
         let autocomplete = new google.maps.places.Autocomplete(
             $('#search-place').get(0),
             {bounds: map.getBounds()}
@@ -33,20 +33,23 @@ var ViewModel = function () {
         });
     });
 
-    $('#search-button').on('click', () => self.searchPlaces());
+    this.search = function () {
+        var address = this.current().trim();
+        if (address) {
+            self.searchPlaces(address);
+        }
+    };
 
     this.searchPlaces = function(address) {
         if (address == null) {
             address = $('#search-place').get(0).value;
         }
-        console.log("addrss = " + address);
         if (address.length = 0) {
             window.alert('You must enter an address.');
             return;
         }
         self.clearMarkers();
         let bounds = map.getBounds();
-        console.log("bounds: " + bounds);
         let placesService = new google.maps.places.PlacesService(map);
         placesService.textSearch({
             query: address,
@@ -62,21 +65,30 @@ var ViewModel = function () {
         let bounds = new google.maps.LatLngBounds();
         for (let i = 0; i < places.length; i++) {
             let place = places[i];
-            let icon = {
-                url: place.icon,
-                size: new google.maps.Size(35, 35),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(15, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-            // Create a marker for each place.
-            let marker = new google.maps.Marker({
-                map: map,
-                //icon: icon,
-                title: place.name,
-                position: place.geometry.location,
-                id: place.place_id
-            });
+            let marker;
+            if (self.use_poi_icon()) {
+                // Create a marker for each place.
+                marker = new google.maps.Marker({
+                    map: map,
+                    icon: {
+                        url: place.icon,
+                        size: new google.maps.Size(35, 35),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(15, 34),
+                        scaledSize: new google.maps.Size(25, 25)
+                    },
+                    title: place.name,
+                    position: place.geometry.location,
+                    id: place.place_id,
+                });
+            } else {
+                marker = new google.maps.Marker({
+                    map: map,
+                    title: place.name,
+                    position: place.geometry.location,
+                    id: place.place_id,
+                });
+            }
     
             var placeInfoWindow = new google.maps.InfoWindow();
             marker.addListener('click', function () {
@@ -95,11 +107,14 @@ var ViewModel = function () {
 
     this.clearMarkers = function() {
         //map.clearOverlays();
-        for (marker of self.markers) {
-            marker.setMap(null);
-        }
-        self.markers = [];
+        self.markers().forEach(marker => marker.setMap(null));
+        self.markers.removeAll();
     }
+
+    self.showPlaceInfo = function (marker) {
+        google.maps.event.trigger(marker, 'click');
+        $('#nav').removeClass('open');  
+    }.bind(this);
 
     this.populateInfoWindow = function(marker, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
@@ -112,14 +127,12 @@ var ViewModel = function () {
                 infowindow.marker = null;
             });
             var streetViewService = new google.maps.StreetViewService();
-            console.log(streetViewService);
             var radius = 50;
             // In case the status is OK, which means the pano was found, compute the
             // position of the streetview image, then calculate the heading, then get a
             // panorama from that and set the options
             function getStreetView(data, status) {
                 if (status == google.maps.StreetViewStatus.OK) {
-                    console.log(".StreetViewStatus.OK");
                     var nearStreetViewLocation = data.location.latLng;
                     var heading = google.maps.geometry.spherical.computeHeading(
                         nearStreetViewLocation, marker.position);
@@ -133,7 +146,6 @@ var ViewModel = function () {
                     };
                     var panorama = new google.maps.StreetViewPanorama($('#pano').get(0), panoramaOptions);
                 } else {
-                    console.log("no street view");
                     infowindow.setContent('<div>' + marker.title + '</div>' +
                         '<div>No Street View Found</div>');
                 }
@@ -144,13 +156,10 @@ var ViewModel = function () {
             // Open the infowindow on the correct marker.
             infowindow.open(map, marker);
         }
-    }
-}
+    };
 
-$(() => {
-    $('#nav_btn').on('click', (event) => {
+    this.toggleNav = function() {
         $('#nav').toggleClass('open');  
         event.stopPropagation();
-    });
-});
-
+    }
+}
